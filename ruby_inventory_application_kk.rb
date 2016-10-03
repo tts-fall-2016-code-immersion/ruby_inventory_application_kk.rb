d60215b4b34f8a88170e8d3bbeb1763742886cce
Katie <newkatiek@gmail.com>
2016-10-03 17:43:21 -0400

    fixed all except loop

--- ruby_inventory_application_kk.rb
+++ ruby_inventory_application_kk.rb
@@ -1,16 +1,16 @@
 donuts_hash = {
-  vanilla: 2,
-  chocolate: 2,
-  creme_filled: 2,
-  strawberry: 2
+  "vanilla" => 2,
+  "chocolate" => 2,
+  "creme_filled" => 2,
+  "strawberry" => 2
 }
 
 puts "What donuts are available to buy today?"
 puts "#{donuts_hash} are fresh and ready to sell."
 
 puts "Not sure 2 vanilla is enough, how many vanilla donuts do you think we need?"
-more_vanilla = gets.chom
-donuts_hash[:vanilla] = more_vanilla
+more_vanilla = gets.chomp.to_i
+donuts_hash["vanilla"] = more_vanilla
 puts "Now we have #{donuts_hash} ready to sell."
 
 puts "What other flavor donuts should we add?"
@@ -21,8 +21,7 @@ puts "Now we have #{donuts_hash} ready to sell."
 
 puts "Now we have too many flavors. Which one should we delete?"
 delete_donut = gets.chomp.downcase
-
-
+donuts_hash.delete(delete_donut)
 puts "Now we have #{donuts_hash} ready to sell."
 
 
